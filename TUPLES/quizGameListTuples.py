78ef6cffb40c9e620b992e3c5519b10d6624477e
radhakrishnanrajan <radhakrishnanrajan@onmyowntechnology.com>
2023-08-06 12:55:48 +0530

    Commit RK
    Commit RK

---
+++ TUPLES/quizGameListTuples.py
@@ -0,0 +1,15 @@
+# quiz.py
+
+#DATA TYPES TUPLES & LIST
+QUESTIONS = [
+    ("1) When was the first known use of the word 'quiz'", "1781"),
+    ("2) Which built-in function can get information from the user", "input"),
+    ("3) Which keyword do you use to loop over a given list of elements", "for")
+]
+
+for question, correct_answer in QUESTIONS:
+    answer = input(f"{question}? ")
+    if answer == correct_answer:
+        print("Correct!")
+    else:
+        print(f"The answer is {correct_answer!r}, not {answer!r}")
